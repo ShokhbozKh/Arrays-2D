ce95a8a79677c2604898be2d6c19fb2726f0e668
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-03 23:28:18 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Arrays_2D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2D massiv elon qilish");
+            int[,] array;
+            array = new int[,] 
+            { 
+                { 1, 2, 3, 4 },
+                { 1, 2, 3, 4},
+                { 1, 2, 3, 4}
+            };
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == j || i>j)
+                    {
+                        array[i, j] = 0;
+                    }
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
